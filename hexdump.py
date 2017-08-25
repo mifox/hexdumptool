63ccf8da0b3bc507b29bd1c871c41ba0f437dffe
dingchaoqun@gmail.com <dingchaoqun@gmail.com>
2017-08-25 17:12:38 +0800

    用python写的hex格式转换小工具： 用于方便协议分析使用：
    已经实现功能：
    1）用于从smartsniffer 复制的hexdump格式 到 c#等语言的二进制数组定义。
    from
    00000000: 00 00 00 5B 68 65 78 64  75 6D 70 5D 00 00 00 00  ...[hexdump]....
    00000010: 00 11 22 33 44 55 66 77  88 99 0A BB CC DD EE FF  .."3DUfw........
    to
     0x00,0x00,0x00,0x5B,0x68,0x65,0x78,0x64,0x75,0x6D,0x70,0x5D,0x00,0x00,0x00,0x00
    ,0x00,0x11,0x22,0x33,0x44,0x55,0x66,0x77,0x88,0x99,0x0A,0xBB,0xCC,0xDD,0xEE,0xFF
    
    2）用于从smartsniffer 复制的hexdump格式 到 sokit 发送数据包。
    from
    00000000: 00 00 00 5B 68 65 78 64  75 6D 70 5D 00 00 00 00  ...[hexdump]....
    00000010: 00 11 22 33 44 55 66 77  88 99 0A BB CC DD EE FF  .."3DUfw........
    to
     0x00,0x00,0x00,0x5B,0x68,0x65,0x78,0x64,0x75,0x6D,0x70,0x5D,0x00,0x00,0x00,0x00
    ,0x00,0x11,0x22,0x33,0x44,0x55,0x66,0x77,0x88,0x99,0x0A,0xBB,0xCC,0xDD,0xEE,0xFF

---
+++ hexdump.py
@@ -0,0 +1,66 @@
+﻿from itertools import chain
+
+class Hexdump:
+    
+    def __init__ (self):
+        self.lineIndex = 0
+    
+        
+    def explain (self,dump):
+          '''
+          Restore binary data from a hex dump.
+            [x] dump argument as a string
+            [ ] dump argument as a line iterator
+        
+          Supported formats:
+            [x] hexdump.hexdump
+            [x] Scapy
+            [x] Far Manager
+          '''
+          minhexwidth = 2*16    # minimal width of the hex part - 00000... style
+          bytehexwidth = 3*16-1 # min width for a bytewise dump - 00 00 ... style
+        
+          #global line
+          text = dump.strip()  # ignore surrounding empty lines
+          #global zeroxfmt
+          zeroxfmt = ''
+          blankfmt = ''
+          for line in text.split('\n'):
+            # strip address part
+              addrend = line.find(':')
+              if 0 < addrend < minhexwidth:  # : is not in ascii part
+                  line = line[addrend+1:]
+                  line = line.lstrip()
+            # check dump type
+              if line[2] == ' ':  # 00 00 00 ...  type of dump
+              # check separator
+                  sepstart = (2+1)*7+2  # ('00'+' ')*7+'00'
+                  sep = line[sepstart:sepstart+3]
+                  #print(line[sepstart:sepstart+3])
+                  if sep[:2] == '  ' and sep[2:] != ' ':  # ...00 00  00 00...双空格
+#                      hexdata = line[:bytehexwidth+1]
+                      hexdata2 = line[:sepstart]
+                      hexdata2 += line[sepstart+1:bytehexwidth+1]
+                  elif sep[2:] == ' ':  # ...00 00 | 00 00...  - Far Manager
+                      hexdata = line[:sepstart] + line[sepstart+3:bytehexwidth+2]
+                  else:                 # ...00 00 00 00... - Scapy, no separator
+                      hexdata = line[:bytehexwidth]
+                  line = hexdata2
+                  #line = line.strip()  # ignore surrounding empty lines
+                  if blankfmt == '':
+                      blankfmt=hexdata2
+                  else:
+                      blankfmt+=" "+hexdata2
+                  if zeroxfmt == '':
+                      zeroxfmt=" 0x"+",0x".join(line.strip().split(' '))+'\n'
+                  else :
+                      zeroxfmt+=",0x"+",0x".join(line.strip().split(' '))
+          #print(zeroxfmt)
+          print(blankfmt)
+          #result += line
+          return zeroxfmt,blankfmt
+		  
+myhexdump = Hexdump ()
+
+#x,y=myhexdump.explain('00000000: 00 00 00 5B 68 65 78 64  75 6D 70 5D 00 00 00 00  ...[hexdump].... \
+#00000010: 00 11 22 33 44 55 66 77  88 99 0A BB CC DD EE FF  .')
